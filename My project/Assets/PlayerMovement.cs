a51b94683f115526571c619df25250dbf99a1d2e
Grey Juliette Desjardins <greydesjardins46@gmail.com>
2025-01-18 16:07:26 -0500

    stashed changes (?)

--- My project/Assets/PlayerMovement.cs
+++ My project/Assets/PlayerMovement.cs
@@ -1,5 +1,8 @@
 using System.Collections;
 using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody2D))]
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -8,6 +11,7 @@ public class PlayerMovement : MonoBehaviour
     private float jumpingPower = 16f;
     private bool isFacingRight = true;
 
+<<<<<<< Updated upstream:My project/Assets/Scripts/Mechanics/PlayerMovement.cs
     private bool canDash = true;
     private bool isDashing;
     private float dashingPower = 24f;
@@ -22,6 +26,37 @@ public class PlayerMovement : MonoBehaviour
     private void Update()
     {
         if (isDashing)
+=======
+    private bool doubleJump;
+
+    private bool canDash = true;
+    private bool isDashing;
+    public float dashingPower = 24f;
+    public float dashingTime = 0.2f; 
+    public float dashingCooldown = 1f;
+
+
+
+    private Rigidbody2D rb;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private TrailRenderer tr; 
+
+
+    void Awake()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+    }
+    
+
+    void Update()
+    {
+
+
+        horizontal = Input.GetAxisRaw("Horizontal");
+
+        if (IsGrounded() && !Input.GetButton("Jump")) 
+>>>>>>> Stashed changes:My project/Assets/PlayerMovement.cs
         {
             return;
         }
@@ -38,9 +73,15 @@ public class PlayerMovement : MonoBehaviour
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, rb.linearVelocity.y * 0.5f);
         }
 
+<<<<<<< Updated upstream:My project/Assets/Scripts/Mechanics/PlayerMovement.cs
         if (Input.GetKeyDown(KeyCode.LeftShift) && canDash)
         {
             StartCoroutine(Dash());
+=======
+        if (Input.GetKeyDown(KeyCode.LeftShift) && canDash && !isDashing)
+        {
+            StartCoroutine(Dash()); 
+>>>>>>> Stashed changes:My project/Assets/PlayerMovement.cs
         }
 
         Flip();
@@ -48,10 +89,13 @@ public class PlayerMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
+<<<<<<< Updated upstream:My project/Assets/Scripts/Mechanics/PlayerMovement.cs
         if (isDashing)
         {
             return;
         }
+=======
+>>>>>>> Stashed changes:My project/Assets/PlayerMovement.cs
 
         rb.linearVelocity = new Vector2(horizontal * speed, rb.linearVelocity.y);
     }
@@ -83,8 +127,22 @@ public class PlayerMovement : MonoBehaviour
         yield return new WaitForSeconds(dashingTime);
         tr.emitting = false;
         rb.gravityScale = originalGravity;
+<<<<<<< Updated upstream:My project/Assets/Scripts/Mechanics/PlayerMovement.cs
         isDashing = false;
         yield return new WaitForSeconds(dashingCooldown);
         canDash = true;
     }
+=======
+        isDashing = false; 
+        yield return new WaitForSeconds(dashingCooldown);
+        canDash = true; 
+    }
+
+
+
+
+
+
+
+>>>>>>> Stashed changes:My project/Assets/PlayerMovement.cs
 }
--- My project/Assets/Scripts/CameraController.cs
+++ My project/Assets/Scripts/CameraController.cs
@@ -2,19 +2,19 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-public Transform target;
-private Vector3 offset;
+	public Transform target;
+	private Vector3 offset;
 
-// Start is called before the first frame update
-void Start()
-{
-offset = transform.position - target.position;
-}
+	// Start is called before the first frame update
+	void Start()
+	{
+		offset = transform.position - target.position;
+	}
 
-// Update is called once per frame
-void Update()
-{
-transform.position = target.position + offset;
-}
+	// Update is called once per frame
+	void LateUpdate()
+	{
+		transform.position = target.position + offset;
+	}
 
 }
