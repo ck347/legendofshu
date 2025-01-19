104dc58426bf9dc3c45dd9c16e2ea9e56dee28a3
Moartae <moartae@gmail.com>
2025-01-19 09:37:47 -0500

    added music/spritechange/fallingspikes script

---
+++ My project/Assets/Scripts/Game/BGMusic.cs
@@ -0,0 +1,20 @@
+using System.Collection;
+using System.Collection.Generic;
+using UnityEngine;
+
+public class BGMusic
+{
+    public static BackgroundMusic_Script instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
