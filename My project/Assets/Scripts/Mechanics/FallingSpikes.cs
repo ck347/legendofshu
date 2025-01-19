32af72a2a8e0a9c007aa7bcd8432e77b6ce17a9e
Moartae <moartae@gmail.com>
2025-01-19 13:59:44 -0500

    hopefully fixe falling spikes

--- My project/Assets/Scripts/Mechanics/FallingSpikes.cs
+++ My project/Assets/Scripts/Mechanics/FallingSpikes.cs
@@ -1,34 +1,57 @@
 using UnityEngine;
+using System.Collections;
+
 
 
 public class FallingSpike : MonoBehaviour
 {
     Vector2 startPos;
+    private float fallDelay = 1f;
+
+    private float respawnDelay = 3f;
+
+        [SerializeField] private Rigidbody2D rb;
+
+
 
     void Start()
     {
     // set the position
         startPos = transform.position;
+        int x = 0;
+        while(x != 10)
+        {
+        StartCoroutine(Fall());
+        x +=1;
+        }
     }      
 
-    void Awake()    
+    // void Awake()    
+    // {
+    //     transform.Translate(0f, -0.1f, 0f);
+        
+    //     yield return new WaitForSeconds(respawnDelay);
+        
+    //     StartCoroutine(Respawn());
+        
+    // }
 
-    {
-        if (transform.position.y >= 4.53)
-        {
-            transform.Translate(0f, -0.1f, 0f);
-        }
-        else
-        {
-            Respawn();
-        }
-    }
+    // IEnumerator Respawn()
+    // {
+    //     transform.position = startPos;
+    // }
 
-    void Respawn()
+
+
+    private IEnumerator Fall()
     {
+        yield return new WaitForSeconds(fallDelay);
+        rb.bodyType = RigidbodyType2D.Dynamic;
+        // Destroy(gameObject, destroyDelay);
+        yield return new WaitForSeconds(respawnDelay);
+        rb.bodyType = RigidbodyType2D.Kinematic;
         transform.position = startPos;
+        
     }
-
-
 }
 
